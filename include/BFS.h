5263a0b5e250e17295308cc39ceb6a872c7e3d9a
Firenis <roman.clenin@gmail.com>
2025-09-21 12:51:20 +0300

    BFS

--- include/BFS.h
+++ include/BFS.h
@@ -4,7 +4,9 @@
 #include "asearch_algorithm.h"
 
 class BFS : public ASearch_algorithm {
-  
+public:
+	BFS();
+	State* search();
 };
 
 #endif //BFS_H
--- src/BFS.cpp
+++ src/BFS.cpp
@@ -1,1 +1,34 @@
 #include "../include/BFS.h"
+
+BFS::BFS() : ASearch_algorithm() {}
+
+
+State* BFS::search() {
+	bool visited;
+	O.push_back(start_state);
+	while (!O.empty()) {
+		State current = O.front();
+		O.pop_front();
+
+		if (current == target_state)
+			return new State(current);
+		visited = false;
+		for (State& element_C : C)
+		{
+			if (current == element_C)
+			{
+				visited = true;
+				break;
+			}
+		}
+		if (!visited)
+		{
+			C.push_back(current);
+			get_child_elements(current);
+		}
+
+	}
+
+	return nullptr;
+
+}
